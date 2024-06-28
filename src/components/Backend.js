d8c21e902d107d1a64a7cb1533e13363eb18a7b0
Nxelia <nxelia99@gmail.com>
2024-06-28 20:26:24 +0200

    languages section modifications

--- src/components/Backend.js
+++ src/components/Backend.js
@@ -2,104 +2,81 @@ import React from "react";
 export const Backend = () => {
     return(
         <div className="skills_content m-auto align-items-center justify-content-center">
-            <h3 className="skills_title purple fw-bold">Technologies</h3>
-            <div className="skills_box">
-                <div className="skills_group">
-                    <div className="skills_data">
-                        <i className='bx bxl-java purple'></i>
-
-                        <div>
-                            <h5 className="skills_name">Java</h5>
-                        </div>
-                    </div>
-
-                    <div className="skills_data">
-                        <i className='bx bxl-python'></i>
-                        <div>
-                            <h5 className="skills_name">Python</h5>
-                        </div>
-                    </div>
-
-                    <div className="skills_data">
-                        <i className='bx bxl-nodejs'></i>
-
-                        <div>
-                            <h5 className="skills_name">Node JS</h5>
-                        </div>
-                    </div>
-
-                    <div className="skills_data">
-                        <i className='bx bx-cylinder'></i>
-                        <div>
-                            <h5 className="skills_name">MySQL</h5>
-                        </div>
-                    </div>
-                    
-                    <div className="skills_data">
-                        <i className='bx bxl-html5'></i>
-                        <div>
-                            <h5 className="skills_name">HTML & CSS</h5>
-                        </div>
-                    </div>
-
-                    <div className="skills_data">
-                        <i className='bx bxl-postgresql'></i>
-                        <div>
-                            <h5 className="skills_name">PostgreSQL</h5>
-                        </div>
-                    </div>
-                    <div className="skills_data">
-                        <i className='bx bxl-javascript'></i>
-                        <div>
-                            <h5 className="skills_name">Javascript</h5>
-                        </div>
-                    </div>
-
-                    <div className="skills_data">
-                        <i className='bx bxl-bootstrap'></i>
-                        <div>
-                            <h5 className="skills_name">Bootstrap</h5>
-                        </div>
-                    </div>
-
-                    <div className="skills_data">
-                        <i className='bx bxl-react'></i>
-                        <div>
-                           <h5 className="skills_name">React</h5>
-                        </div>
-                    </div>    
-                    <div className="skills_data">
-                        <i class='bx bxl-mongodb'></i>
-                        <div>
-                           <h5 className="skills_name">MongoDB</h5>
-                        </div>
-                    </div>
-                    <div className="skills_data">  
-                        <i class='bx bxs-server' ></i>
-                        <div>
-                           <h5 className="skills_name">FastAPI</h5>
-                        </div>
-                    </div>  
-                    <div className="skills_data">  
-                        <i class='bx bxl-django'></i>
-                        <div>
-                           <h5 className="skills_name">Django</h5>
-                        </div>
-                    </div>
-                    <div className="skills_data">
-                        <i className='bx bxl-git'></i>
-                        <div>
-                            <h5 className="skills_name">Git</h5>
-                        </div>
-                    </div>
-                    <div className="skills_data">
-                        <i className='bx bxl-docker'></i>
-                        <div>
-                            <h5 className="skills_name">Docker</h5>
-                        </div>
-                    </div>
+        <h3 className="skills_title purple fw-bold">Technologies</h3>
+        <div className="skills_box">
+            <div className="skills_group">
+                <div className="skills_data">
+                    <i className='bx bxl-java purple'></i>
+                    <h5 className="skills_name">Java</h5>
                 </div>
-            </div> 
+    
+                <div className="skills_data">
+                    <i className='bx bxl-python'></i>
+                    <h5 className="skills_name">Python</h5>
+                </div>
+    
+                <div className="skills_data">
+                    <i className='bx bxl-nodejs'></i>
+                    <h5 className="skills_name">Node JS</h5>
+                </div>
+    
+                <div className="skills_data">
+                    <i className='bx bx-cylinder'></i>
+                    <h5 className="skills_name">MySQL</h5>
+                </div>
+    
+                <div className="skills_data">
+                    <i className='bx bxl-html5'></i>
+                    <h5 className="skills_name">HTML & CSS</h5>
+                </div>
+    
+                <div className="skills_data">
+                    <i className='bx bxl-postgresql'></i>
+                    <h5 className="skills_name">PostgreSQL</h5>
+                </div>
+    
+                <div className="skills_data">
+                    <i className='bx bxl-javascript'></i>
+                    <h5 className="skills_name">JavaScript</h5>
+                </div>
+    
+                <div className="skills_data">
+                    <i className='bx bxl-bootstrap'></i>
+                    <h5 className="skills_name">Bootstrap</h5>
+                </div>
+    
+                <div className="skills_data">
+                    <i className='bx bxl-react'></i>
+                    <h5 className="skills_name">React</h5>
+                </div>    
+    
+                <div className="skills_data">
+                    <i className='bx bxl-mongodb'></i>
+                    <h5 className="skills_name">MongoDB</h5>
+                </div>
+    
+                <div className="skills_data">  
+                    <i className='bx bxs-server'></i>
+                    <h5 className="skills_name">FastAPI</h5>
+                </div>  
+    
+                <div className="skills_data">  
+                    <i className='bx bxl-django'></i>
+                    <h5 className="skills_name">Django</h5>
+                </div>
+    
+                <div className="skills_data">
+                    <i className='bx bxl-git'></i>
+                    <h5 className="skills_name">Git</h5>
+                </div>
+    
+                <div className="skills_data">
+                    <i className='bx bxl-docker'></i>
+                    <h5 className="skills_name">Docker</h5>
+                </div>
+            </div>
         </div> 
+    </div> 
+    
     )
 }
